c248df1cefc25e73ec71fd3f8974d856aa8b6d2a
William Lin <linwilliam14@gmail.com>
2026-05-04 08:04:39 +0200

    feat(frontend): brancher ThemeProvider dans main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
+import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
@@ -11,9 +12,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ToastProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ThemeProvider>
       </ToastProvider>
     </BrowserRouter>
   </StrictMode>
